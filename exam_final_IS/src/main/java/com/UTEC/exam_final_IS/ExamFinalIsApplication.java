e19b75de0ff2af5e842091527075180dd5042311
jose <jose.sanchez.a@utec.edu.pe>
2021-07-26 16:37:17 -0500

    inicio

---
+++ exam_final_IS/src/main/java/com/UTEC/exam_final_IS/ExamFinalIsApplication.java
@@ -0,0 +1,13 @@
+package com.UTEC.exam_final_IS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamFinalIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamFinalIsApplication.class, args);
+	}
+
+}
---
+++ exam_final_IS/src/test/java/com/UTEC/exam_final_IS/ExamFinalIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UTEC.exam_final_IS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamFinalIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
